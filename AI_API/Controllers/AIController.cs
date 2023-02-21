5afff26339757a9a9f961e5cee67940bf82f24d2
Mateusz <82496006+MGabala@users.noreply.github.com>
2023-02-21 19:27:15 +0100

    Add project files.

---
+++ AI_API/Controllers/AIController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using RestSharp;
+
+namespace AI_API.Controllers
+{
+    [ApiController]
+    [Route("modern-it.pl/ai")]
+    public class AIController : ControllerBase
+    {
+        private readonly string APIURL = "https://api.openai.com/";
+
+        [HttpGet,Route("getallmodules")]
+        public async Task<IActionResult> GetAllModels()
+        {
+
+            var client = new RestClient(APIURL);
+            var request = new RestRequest("/v1/models", Method.Get);
+            request.AddHeader("Authorization", $"Bearer {Environment.GetEnvironmentVariable("OPENAIKEY")}");
+            RestResponse response = await client.ExecuteAsync(request);
+            return Ok(response.Content);
+        }
+    }
+}
---
+++ AI_API/Program.cs
@@ -0,0 +1,36 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+///Logger
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Hour)
+    .CreateLogger();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseSerilogRequestLogging();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
